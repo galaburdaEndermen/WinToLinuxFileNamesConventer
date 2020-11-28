cf27e7b46c1581b54dd50afa69425089de06f910
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-11-28 16:33:24 +0300

    initial2

---
+++ WinToLinuxFileNamesConventer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinToLinuxFileNamesConventer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
